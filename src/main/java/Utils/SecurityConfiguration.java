03777c44203407e258e9f3745d5d5f0a999b32e3
HumaMilisic <huma.milisic@gmail.com>
2016-03-08 20:22:57 +0100

    postavke za openshift

--- src/main/java/Utils/SecurityConfiguration.java
+++ src/main/java/Utils/SecurityConfiguration.java
@@ -1,13 +1,13 @@
-package Utils;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
-    }
-}
+//package Utils;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception{
+//        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+//    }
+//}
