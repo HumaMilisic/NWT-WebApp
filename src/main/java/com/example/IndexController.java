953808ecc117eb290e68d807c06e2cd5c9defa8a
HumaMilisic <huma.milisic@gmail.com>
2016-03-10 19:07:49 +0100

    testiranje za openshift

--- src/main/java/com/example/IndexController.java
+++ src/main/java/com/example/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @RequestMapping("/")
     public String index(){
-        return "index test github push";
+
+        return "index test";
     }
 }
