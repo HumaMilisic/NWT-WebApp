bcd2a21b2cf0760c452386507e11499c1643f795
Dina <dinaahmic@hotmail.com>
2016-03-27 18:01:19 +0200

    pokusaj - notifikacija, relacija dokument, status, relacija korisnik

--- src/main/java/com/example/models/RelacijaKorisnik.java
+++ src/main/java/com/example/models/RelacijaKorisnik.java
@@ -1,5 +1,9 @@
 package com.example.models;
 
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.validator.constraints.Length;
+
 import java.io.Serializable;
 import javax.persistence.*;
 import java.util.List;
@@ -10,14 +14,15 @@ import java.util.List;
  * 
  */
 @Entity
-@Table(name="RELACIJA_KORISNIK")
-@NamedQuery(name="RelacijaKorisnik.findAll", query="SELECT r FROM RelacijaKorisnik r")
+//@Table(name="RELACIJA_KORISNIK")
+//@NamedQuery(name="RelacijaKorisnik.findAll", query="SELECT r FROM RelacijaKorisnik r")
 public class RelacijaKorisnik implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "RelacijaKorisnikSEQ")
 	@SequenceGenerator(name = "RelacijaKorisnikSEQ", sequenceName = "RELACIJA_KORISNIK_SEQ", allocationSize = 1)
+	@Generated(GenerationTime.INSERT) //dodano
 	private long id;
 
 	private String nazivba;
@@ -25,26 +30,29 @@ public class RelacijaKorisnik implements Serializable {
 	private String naziven;
 
 	//bi-directional many-to-one association to KorisnikXKorisnik
-	@OneToMany(mappedBy="relacijaKorisnik")
-	private List<KorisnikXKorisnik> korisnikXKorisniks;
+//	@OneToMany(mappedBy="relacijaKorisnik")
+//	private List<KorisnikXKorisnik> korisnikXKorisniks;
 
 	//bi-directional many-to-one association to RelKorisnikXNotifikacija
-	@OneToMany(mappedBy="relacijaKorisnikBean")
-	private List<RelKorisnikXNotifikacija> relKorisnikXNotifikacijas;
+//	@OneToMany(mappedBy="relacijaKorisnikBean")
+//	private List<RelKorisnikXNotifikacija> relKorisnikXNotifikacijas;
 
 	//bi-directional many-to-one association to RelKorisnikXRelDokument
-	@OneToMany(mappedBy="relacijaKorisnikBean")
-	private List<RelKorisnikXRelDokument> relKorisnikXRelDokuments;
+//	@OneToMany(mappedBy="relacijaKorisnikBean")
+//	private List<RelKorisnikXRelDokument> relKorisnikXRelDokuments;
 
 	//bi-directional many-to-one association to RelKorisnikXStatus
-	@OneToMany(mappedBy="relacijaKorisnikBean")
-	private List<RelKorisnikXStatus> relKorisnikXStatuses;
+//	@OneToMany(mappedBy="relacijaKorisnikBean")
+//	private List<RelKorisnikXStatus> relKorisnikXStatuses;
 
 	//bi-directional many-to-one association to RelKorisnikXVrstaDoc
-	@OneToMany(mappedBy="relacijaKorisnikBean")
-	private List<RelKorisnikXVrstaDoc> relKorisnikXVrstaDocs;
+//	@OneToMany(mappedBy="relacijaKorisnikBean")
+//	private List<RelKorisnikXVrstaDoc> relKorisnikXVrstaDocs;
 
-	private char deleted;
+//	private char deleted;
+	@Length(max = 1)
+	@Column(nullable = false,name="deleted")
+	private String deleted;
 
 	public RelacijaKorisnik() {
 	}
@@ -73,122 +81,119 @@ public class RelacijaKorisnik implements Serializable {
 		this.naziven = naziven;
 	}
 
-	public List<KorisnikXKorisnik> getKorisnikXKorisniks() {
-		return this.korisnikXKorisniks;
-	}
+//	public List<KorisnikXKorisnik> getKorisnikXKorisniks() {
+//		return this.korisnikXKorisniks;
+//	}
 
-	public void setKorisnikXKorisniks(List<KorisnikXKorisnik> korisnikXKorisniks) {
-		this.korisnikXKorisniks = korisnikXKorisniks;
-	}
+//	public void setKorisnikXKorisniks(List<KorisnikXKorisnik> korisnikXKorisniks) {
+//		this.korisnikXKorisniks = korisnikXKorisniks;
+//	}
 
-	public KorisnikXKorisnik addKorisnikXKorisnik(KorisnikXKorisnik korisnikXKorisnik) {
-		getKorisnikXKorisniks().add(korisnikXKorisnik);
-		korisnikXKorisnik.setRelacijaKorisnik(this);
+//	public KorisnikXKorisnik addKorisnikXKorisnik(KorisnikXKorisnik korisnikXKorisnik) {
+//		getKorisnikXKorisniks().add(korisnikXKorisnik);
+//		korisnikXKorisnik.setRelacijaKorisnik(this);
+//		return korisnikXKorisnik;
+//	}
 
-		return korisnikXKorisnik;
-	}
+//	public KorisnikXKorisnik removeKorisnikXKorisnik(KorisnikXKorisnik korisnikXKorisnik) {
+//		getKorisnikXKorisniks().remove(korisnikXKorisnik);
+//		korisnikXKorisnik.setRelacijaKorisnik(null);
+//		return korisnikXKorisnik;
+//	}
 
-	public KorisnikXKorisnik removeKorisnikXKorisnik(KorisnikXKorisnik korisnikXKorisnik) {
-		getKorisnikXKorisniks().remove(korisnikXKorisnik);
-		korisnikXKorisnik.setRelacijaKorisnik(null);
+//	public List<RelKorisnikXNotifikacija> getRelKorisnikXNotifikacijas() {
+//		return this.relKorisnikXNotifikacijas;
+//	}
 
-		return korisnikXKorisnik;
-	}
+//	public void setRelKorisnikXNotifikacijas(List<RelKorisnikXNotifikacija> relKorisnikXNotifikacijas) {
+//		this.relKorisnikXNotifikacijas = relKorisnikXNotifikacijas;
+//	}
 
-	public List<RelKorisnikXNotifikacija> getRelKorisnikXNotifikacijas() {
-		return this.relKorisnikXNotifikacijas;
-	}
+//	public RelKorisnikXNotifikacija addRelKorisnikXNotifikacija(RelKorisnikXNotifikacija relKorisnikXNotifikacija) {
+//		getRelKorisnikXNotifikacijas().add(relKorisnikXNotifikacija);
+//		relKorisnikXNotifikacija.setRelacijaKorisnikBean(this);
+//		return relKorisnikXNotifikacija;
+//	}
 
-	public void setRelKorisnikXNotifikacijas(List<RelKorisnikXNotifikacija> relKorisnikXNotifikacijas) {
-		this.relKorisnikXNotifikacijas = relKorisnikXNotifikacijas;
-	}
+//	public RelKorisnikXNotifikacija removeRelKorisnikXNotifikacija(RelKorisnikXNotifikacija relKorisnikXNotifikacija) {
+//		getRelKorisnikXNotifikacijas().remove(relKorisnikXNotifikacija);
+//		relKorisnikXNotifikacija.setRelacijaKorisnikBean(null);
+//		return relKorisnikXNotifikacija;
+//	}
 
-	public RelKorisnikXNotifikacija addRelKorisnikXNotifikacija(RelKorisnikXNotifikacija relKorisnikXNotifikacija) {
-		getRelKorisnikXNotifikacijas().add(relKorisnikXNotifikacija);
-		relKorisnikXNotifikacija.setRelacijaKorisnikBean(this);
+//	public List<RelKorisnikXRelDokument> getRelKorisnikXRelDokuments() {
+//		return this.relKorisnikXRelDokuments;
+//	}
 
-		return relKorisnikXNotifikacija;
-	}
+//	public void setRelKorisnikXRelDokuments(List<RelKorisnikXRelDokument> relKorisnikXRelDokuments) {
+//		this.relKorisnikXRelDokuments = relKorisnikXRelDokuments;
+//	}
 
-	public RelKorisnikXNotifikacija removeRelKorisnikXNotifikacija(RelKorisnikXNotifikacija relKorisnikXNotifikacija) {
-		getRelKorisnikXNotifikacijas().remove(relKorisnikXNotifikacija);
-		relKorisnikXNotifikacija.setRelacijaKorisnikBean(null);
+//	public RelKorisnikXRelDokument addRelKorisnikXRelDokument(RelKorisnikXRelDokument relKorisnikXRelDokument) {
+//		getRelKorisnikXRelDokuments().add(relKorisnikXRelDokument);
+//		relKorisnikXRelDokument.setRelacijaKorisnikBean(this);
+//		return relKorisnikXRelDokument;
+//	}
 
-		return relKorisnikXNotifikacija;
-	}
+//	public RelKorisnikXRelDokument removeRelKorisnikXRelDokument(RelKorisnikXRelDokument relKorisnikXRelDokument) {
+//		getRelKorisnikXRelDokuments().remove(relKorisnikXRelDokument);
+//		relKorisnikXRelDokument.setRelacijaKorisnikBean(null);
+//		return relKorisnikXRelDokument;
+//	}
 
-	public List<RelKorisnikXRelDokument> getRelKorisnikXRelDokuments() {
-		return this.relKorisnikXRelDokuments;
-	}
+//	public List<RelKorisnikXStatus> getRelKorisnikXStatuses() {
+//		return this.relKorisnikXStatuses;
+//	}
 
-	public void setRelKorisnikXRelDokuments(List<RelKorisnikXRelDokument> relKorisnikXRelDokuments) {
-		this.relKorisnikXRelDokuments = relKorisnikXRelDokuments;
-	}
+//	public void setRelKorisnikXStatuses(List<RelKorisnikXStatus> relKorisnikXStatuses) {
+//		this.relKorisnikXStatuses = relKorisnikXStatuses;
+//	}
 
-	public RelKorisnikXRelDokument addRelKorisnikXRelDokument(RelKorisnikXRelDokument relKorisnikXRelDokument) {
-		getRelKorisnikXRelDokuments().add(relKorisnikXRelDokument);
-		relKorisnikXRelDokument.setRelacijaKorisnikBean(this);
+//	public RelKorisnikXStatus addRelKorisnikXStatus(RelKorisnikXStatus relKorisnikXStatus) {
+//		getRelKorisnikXStatuses().add(relKorisnikXStatus);
+//		relKorisnikXStatus.setRelacijaKorisnikBean(this);
+//		return relKorisnikXStatus;
+//	}
 
-		return relKorisnikXRelDokument;
-	}
+//	public RelKorisnikXStatus removeRelKorisnikXStatus(RelKorisnikXStatus relKorisnikXStatus) {
+//		getRelKorisnikXStatuses().remove(relKorisnikXStatus);
+//		relKorisnikXStatus.setRelacijaKorisnikBean(null);
+//		return relKorisnikXStatus;
+//	}
 
-	public RelKorisnikXRelDokument removeRelKorisnikXRelDokument(RelKorisnikXRelDokument relKorisnikXRelDokument) {
-		getRelKorisnikXRelDokuments().remove(relKorisnikXRelDokument);
-		relKorisnikXRelDokument.setRelacijaKorisnikBean(null);
+//	public List<RelKorisnikXVrstaDoc> getRelKorisnikXVrstaDocs() {
+//		return this.relKorisnikXVrstaDocs;
+//	}
 
-		return relKorisnikXRelDokument;
-	}
+//	public void setRelKorisnikXVrstaDocs(List<RelKorisnikXVrstaDoc> relKorisnikXVrstaDocs) {
+//		this.relKorisnikXVrstaDocs = relKorisnikXVrstaDocs;
+//	}
 
-	public List<RelKorisnikXStatus> getRelKorisnikXStatuses() {
-		return this.relKorisnikXStatuses;
-	}
+//	public RelKorisnikXVrstaDoc addRelKorisnikXVrstaDoc(RelKorisnikXVrstaDoc relKorisnikXVrstaDoc) {
+//		getRelKorisnikXVrstaDocs().add(relKorisnikXVrstaDoc);
+//		relKorisnikXVrstaDoc.setRelacijaKorisnikBean(this);
+//		return relKorisnikXVrstaDoc;
+//	}
 
-	public void setRelKorisnikXStatuses(List<RelKorisnikXStatus> relKorisnikXStatuses) {
-		this.relKorisnikXStatuses = relKorisnikXStatuses;
-	}
+//	public RelKorisnikXVrstaDoc removeRelKorisnikXVrstaDoc(RelKorisnikXVrstaDoc relKorisnikXVrstaDoc) {
+//		getRelKorisnikXVrstaDocs().remove(relKorisnikXVrstaDoc);
+//		relKorisnikXVrstaDoc.setRelacijaKorisnikBean(null);
+//		return relKorisnikXVrstaDoc;
+//	}
 
-	public RelKorisnikXStatus addRelKorisnikXStatus(RelKorisnikXStatus relKorisnikXStatus) {
-		getRelKorisnikXStatuses().add(relKorisnikXStatus);
-		relKorisnikXStatus.setRelacijaKorisnikBean(this);
-
-		return relKorisnikXStatus;
-	}
-
-	public RelKorisnikXStatus removeRelKorisnikXStatus(RelKorisnikXStatus relKorisnikXStatus) {
-		getRelKorisnikXStatuses().remove(relKorisnikXStatus);
-		relKorisnikXStatus.setRelacijaKorisnikBean(null);
-
-		return relKorisnikXStatus;
-	}
-
-	public List<RelKorisnikXVrstaDoc> getRelKorisnikXVrstaDocs() {
-		return this.relKorisnikXVrstaDocs;
-	}
-
-	public void setRelKorisnikXVrstaDocs(List<RelKorisnikXVrstaDoc> relKorisnikXVrstaDocs) {
-		this.relKorisnikXVrstaDocs = relKorisnikXVrstaDocs;
-	}
-
-	public RelKorisnikXVrstaDoc addRelKorisnikXVrstaDoc(RelKorisnikXVrstaDoc relKorisnikXVrstaDoc) {
-		getRelKorisnikXVrstaDocs().add(relKorisnikXVrstaDoc);
-		relKorisnikXVrstaDoc.setRelacijaKorisnikBean(this);
-
-		return relKorisnikXVrstaDoc;
-	}
-
-	public RelKorisnikXVrstaDoc removeRelKorisnikXVrstaDoc(RelKorisnikXVrstaDoc relKorisnikXVrstaDoc) {
-		getRelKorisnikXVrstaDocs().remove(relKorisnikXVrstaDoc);
-		relKorisnikXVrstaDoc.setRelacijaKorisnikBean(null);
-
-		return relKorisnikXVrstaDoc;
-	}
-
-	public char getDeleted() {
+/*	public char getDeleted() {
 		return this.deleted;
 	}
 
 	public void setDeleted(char deleted) {
 		this.deleted = deleted;
 	}
+*/
+	public String getDeleted() { //bilo char
+	return this.deleted;
+}
 
+	public void setDeleted(String deleted) {
+		this.deleted = deleted;
+	}
 }
---
+++ src/main/java/com/example/repo/RelacijaKorisnikRepository.java
@@ -0,0 +1,59 @@
+package com.example.repo;
+
+import com.example.models.RelacijaKorisnik;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
+
+@PreAuthorize("hasRole('ROLE_USER')")
+@RepositoryRestResource(collectionResourceRel = "relacijaKorisnik",path = "relacijaKorisnik")
+public interface RelacijaKorisnikRepository extends PagingAndSortingRepository<RelacijaKorisnik,Long> {
+
+    @Override
+    Iterable<RelacijaKorisnik> findAll(Sort sort);
+
+    @Override
+    Page<RelacijaKorisnik> findAll(Pageable pageable);
+
+    @Override
+    RelacijaKorisnik save(RelacijaKorisnik s);
+
+    @Override
+    RelacijaKorisnik findOne(Long aLong);
+
+    @Override
+    boolean exists(Long aLong);
+
+    @Override
+    Iterable<RelacijaKorisnik> findAll();
+
+    @Override
+    Iterable<RelacijaKorisnik> findAll(Iterable<Long> iterable);
+
+    @Override
+    long count();
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void delete(Long aLong);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void delete(RelacijaKorisnik relacijaKorisnik);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void delete(Iterable<? extends RelacijaKorisnik> iterable);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void deleteAll();
+
+    List<RelacijaKorisnik> findById(@Param("id")long id); //zasad po id
+}
