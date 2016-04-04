5ba30652e2b067394ba547d0eebb5951766a37bf
HumaMilisic <huma.milisic@gmail.com>
2016-04-04 20:08:16 +0200

    ... unistena prethodna verzija slucajno, random password generator

---
+++ src/main/java/com/example/utils/GlobalStuff.java
@@ -0,0 +1,36 @@
+package com.example.utils;
+
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
+
+import java.util.Arrays;
+
+/**
+ * Created by WorkIt on 04/04/2016.
+ */
+public class GlobalStuff {
+//    public static List<CharacterRule> getRules(){
+////        return new List<CharacterRule>(
+////                new LengthRule(8,30),
+////                new CharacterRule(EnglishCharacterData.UpperCase,1),
+////                new CharacterRule(EnglishCharacterData.Digit,1),
+////                new CharacterRule(EnglishCharacterData.Special,1)//,
+//////                new UppercaseCharacterRule(1),
+//////                new DigitCharacterRule(1),
+//////                new SpecialCharacterRule(1),
+//////                new WhitespaceRule()
+////        );
+//    }
+    private static PasswordGenerator generator;
+
+    public static String RandomPassword(){
+        String rez = generator.generatePassword(11,Arrays.asList(
+                new CharacterRule(EnglishCharacterData.UpperCase,1),
+                new CharacterRule(EnglishCharacterData.LowerCase,4),
+                new CharacterRule(EnglishCharacterData.Digit,5),
+                new CharacterRule(EnglishCharacterData.Special,1)
+        ));
+        return rez;
+    }
+}
