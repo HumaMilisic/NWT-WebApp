41666d9e3c0e9269299f5574e4bd766010b8adc6
HumaMilisic <huma.milisic@gmail.com>
2016-06-01 20:20:06 +0200

    svasta skinuto

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,10 +2,8 @@ package com.example;
 
 import com.example.metrics.AuthMetricService;
 import com.example.metrics.IAuthMetricService;
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,12 +24,12 @@ public class DemoApplication {
 		return new BCryptPasswordEncoder(11);
 	}
 
-	@Bean
-	public ServletRegistrationBean h2servletRegistration(){
-		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-		registrationBean.addUrlMappings("/console/*");
-		return registrationBean;
-	}
+//	@Bean
+//	public ServletRegistrationBean h2servletRegistration(){
+//		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+//		registrationBean.addUrlMappings("/console/*");
+//		return registrationBean;
+//	}
 
 	@Bean
 	//    @Autowired
--- src/main/java/com/example/config/MvcConfig.java
+++ src/main/java/com/example/config/MvcConfig.java
@@ -1,24 +1,24 @@
-package com.example.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * Created by WorkIt on 12/03/2016.
- */
-@Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter{
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry){
-//        registry.addViewController("/index").setViewName("/index");
-        registry.addViewController("/hello").setViewName("hello");
-//        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/login").setViewName("login");
-//        registry
-        registry.addViewController("/registrationConfirm").setViewName("registrationConfirm");
-//        registry.addViewController("/register").setViewName("register");
-
+//package com.example.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * Created by WorkIt on 12/03/2016.
+// */
+//@Configuration
+//public class MvcConfig extends WebMvcConfigurerAdapter{
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry){
+////        registry.addViewController("/index").setViewName("/index");
 //        registry.addViewController("/hello").setViewName("hello");
-    }
-}
+////        registry.addViewController("/").setViewName("index");
+//        registry.addViewController("/login").setViewName("login");
+////        registry
+//        registry.addViewController("/registrationConfirm").setViewName("registrationConfirm");
+////        registry.addViewController("/register").setViewName("register");
+//
+////        registry.addViewController("/hello").setViewName("hello");
+//    }
+//}
