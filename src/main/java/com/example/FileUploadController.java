d6c04c790cb30b08d8abfd027d4a80894b0fad8a
MerimaKlico <mklico1@etf.unsa.ba>
2016-06-07 00:35:39 +0200

    Magic number provjera

--- src/main/java/com/example/FileUploadController.java
+++ src/main/java/com/example/FileUploadController.java
@@ -37,10 +37,11 @@ import com.asprise.ocr.Ocr;
 //ako ovo zakomentarišem ne može ga naći
 @Controller
 public class FileUploadController { //extends HttpServlet {
+    private static boolean OCRinit = false;
     private DokumentRepository repo;
 
     private static final String filesRoot = System.getenv("OPENSHIFT_DATA_DIR");
-    //private static final String filesRoot = "F:\\appDocs";
+    //private static final String filesRoot = "F:/appDocs";
 
     @RequestMapping(method = RequestMethod.GET, value = "/document") //, produces = MediaType.APPLICATION_JSON_VALUE)
     //public String provideUploadInfo(Model model) {
@@ -113,7 +114,7 @@ public class FileUploadController { //extends HttpServlet {
     public void getDownload(@PathVariable String fileName, HttpServletResponse response) { //HttpServletResponse response, HttpServletRequest request) {
 
         // Get your file stream from wherever.
-        File rootFolder = new File(filesRoot + "\\" + SecurityContextHolder.getContext().getAuthentication().getName());
+        File rootFolder = new File(filesRoot + "/" + SecurityContextHolder.getContext().getAuthentication().getName());
 
         //debug
         /*try {
@@ -129,7 +130,7 @@ public class FileUploadController { //extends HttpServlet {
         //
 
 
-        File file = new File(rootFolder + "\\" + fileName); //debug// + "\\" + request.getPathInfo().substring(1));
+        File file = new File(rootFolder + "/" + fileName); //debug// + "\\" + request.getPathInfo().substring(1));
 
         InputStream myStream;
         try {
@@ -221,27 +222,48 @@ public class FileUploadController { //extends HttpServlet {
 
         if (!file.isEmpty()) {
             try {
-                File usersFolder = new File(filesRoot + "\\" + SecurityContextHolder.getContext().getAuthentication().getName());
+                File usersFolder = new File(filesRoot + "/" + SecurityContextHolder.getContext().getAuthentication().getName());
                 if(!usersFolder.exists()) usersFolder.mkdirs();
 
-                File f = new File(usersFolder + "\\" + name);
-
-                Ocr.setUp();
-                Ocr ocr = new Ocr();
-                ocr.startEngine("eng", Ocr.SPEED_FASTEST);
+                File f = new File(usersFolder + "/" + name);
 
                 BufferedOutputStream stream = new BufferedOutputStream(
                         //new FileOutputStream(new File(Application.ROOT + "/" + name)));
                         //new FileOutputStream(new File(filesRoot + "/" + name)));
-                        new FileOutputStream(new File(filesRoot + "\\" + SecurityContextHolder.getContext().getAuthentication().getName() + "\\" + name)));
+                        new FileOutputStream(new File(filesRoot + "/" + SecurityContextHolder.getContext().getAuthentication().getName() + "/" + name)));
                 FileCopyUtils.copy(file.getInputStream(), stream);
 
 
                 stream.close();
 
-                ocr.recognize(new File[] {f},
-                        Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PDF, Ocr.PROP_PDF_OUTPUT_FILE, filesRoot + "\\" + SecurityContextHolder.getContext().getAuthentication().getName() + "\\" + name, Ocr.PROP_PDF_OUTPUT_TEXT_VISIBLE, false);
-                ocr.stopEngine();
+                //OCR samo za PDF
+                FileInputStream test = new FileInputStream(f);
+                byte[] signature = new byte[4];
+                test.read(signature);
+                String ascii = new String(signature);
+                //for(int i = 0; i < signature.length; i++) {
+                //    ascii[i] = signature[i];
+                //}
+                //for(int i = 0; i < signature.length; i++) {
+                //    String x = Integer.toHexString(signature[i]);
+                //    signature[i] = Byte.valueOf(x, 16);
+                //}
+                //String stringsignature = signature.toString();
+                if(ascii.equals("%PDF")) {
+                    if(!OCRinit) {
+                        Ocr.setUp();
+                        OCRinit = true;
+                    }
+                    Ocr ocr = new Ocr();
+                    ocr.startEngine("eng", Ocr.SPEED_FASTEST);
+                    ocr.recognize(new File[] {f},
+                            Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PDF,
+                            Ocr.PROP_PDF_OUTPUT_FILE, filesRoot + "/" + SecurityContextHolder.getContext().getAuthentication().getName() + "/" + name,
+                            Ocr.PROP_PDF_OUTPUT_TEXT_VISIBLE, false);
+                    ocr.stopEngine();
+                }
+
+
                 //redirectAttributes.addFlashAttribute("message",
                 //        "You successfully uploaded " + name + "!");
             }
@@ -264,8 +286,8 @@ public class FileUploadController { //extends HttpServlet {
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/document/{fileName:.+}")
     public @ResponseBody String delete(@PathVariable String fileName) {
-        File rootFolder = new File(filesRoot + "\\" + SecurityContextHolder.getContext().getAuthentication().getName());
-        File file = new File(rootFolder + "\\" + fileName);
+        File rootFolder = new File(filesRoot + "/" + SecurityContextHolder.getContext().getAuthentication().getName());
+        File file = new File(rootFolder + "/" + fileName);
         if(file.delete())
             return "{ \"status\": \"Fajl je uspjesno obrisan!\" }";
         else
